eea0b2918f3b3d73d79cf3914d111f472586f169
Gediminas Kirsanskas <geki@dhigroup.com>
2024-05-05 00:00:33 +0200

    Test for MacOS build.

--- scripts/cibw_before_all_macos.sh
+++ scripts/cibw_before_all_macos.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e -x
 
-ln -sf /usr/local/bin/gcc-10 /usr/local/bin/gcc
+GCC_VERSION=gcc-12
 
+echo "Check the locations compilers"
+whereis gcc
+whereis clang
+whereis $GCC_VERSION
+
+echo "Determine the location of $GCC_VERSION"
+GCC12_PATH=$(type -P $GCC_VERSION)
+echo $GCC12_PATH
+
+echo "Link gcc command to particular $GCC_VERSION"
+ln -sf $GCC12_PATH /usr/local/bin/gcc
 gcc --version
+
+echo "Link clang command to particular $GCC_VERSION"
+ln -sf $GCC12_PATH /usr/local/bin/clang
+clang --version
